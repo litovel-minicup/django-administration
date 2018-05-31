d764ce12a8a558019d916469a5dd75892a55600f
Josef Kolář <thejoeejoee@gmail.com>
2018-05-31 22:05:37 +0200

    Add Facebook comments publisher

--- minicup_model/minicup/settings_default.py
+++ minicup_model/minicup/settings_default.py
@@ -129,3 +129,5 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+FACEBOOK_PAGE_ACCESS_TOKEN = ''
